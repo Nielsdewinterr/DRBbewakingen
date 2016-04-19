148e4ed4b6420638cff9cca5116fceab01e65a08
Nielsdewinterr <niels@dewinterr.nl>
2016-04-19 21:13:39 +0200

    sd

--- Nieuweaanvraag.php
+++ Nieuweaanvraag.php
@@ -1,1 +1,26 @@
-
+<html>
+<head>
+</head>
+<title>Aanvraag Reddingsbrigade</title>
+</head>
+<body class="homepage"> 
+    <form method="POST" action="indienen.php">
+     Naam evenement:    <input type="text" name="Naam" size="40"><br>
+    Naam Organisatie  <input type="text" name="Organisatie" size="40"><br>
+    Telefoonnummer contact persoon voor evenement:    <input type="text" name="Telefoon" size="40"><br>
+    Email adres:    <input type="text" name="Email" size="40"><br>
+     Datum evenement:   <input type="text" name="Datum_evenement" size="40"><br>
+    Type evenement:    <input type="text" name="Type_evenement" size="40"><br>
+    Begin tijd:  <input type="text" name="Tijd_van" size="40"><br>
+     Eind tijd   <input type="text" name="Tijd_tot" size="40"><br>
+    Straat evenement    <input type="text" name="Straat_evenement" size="40"><br>
+     Huisnummer evenement:   <input type="text" name="Huisnummer_evenement" size="40"><br>
+      Postcode evenement:  <input type="text" name="Postcode_evenement" size="40"><br>
+      Plaats evenement:   <input type="text" name="Plaats_evenement" size="40"><br>
+      Postadres opdrachtgever:  <input type="text" name="Straat_opdracht" size="40"><br>
+      Postcode opdrachtgever:  <input type="text" name="Postcode_opdracht" size="40"><br>
+    Plaats opdrachtgever: <input type="text" name="Plaats_opdracht" size="40"><br>  
+       <input id="button" type="submit" name="submit" value="Verstuur"> 
+    </form>
+    </body>
+</html>
